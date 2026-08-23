8f4eea306d0ab3a5717cf87063ec6048db848c1f
Turag347 <turagkhan347@gmail.com>
2026-08-24 00:55:59 +1000

    Update Android Gradle plugin version to 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.7.3"
+    id("com.android.application") version "8.11.1"
     id("org.jetbrains.kotlin.android") version "2.0.21"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
